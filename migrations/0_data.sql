f9aaeec51360f93952c03c6e102a646772a025d1
Siraaj Khandkar <siraaj@khandkar.net>
2024-08-03 00:56:18 -0400

    Add indexes

--- migrations/0_data.sql
+++ migrations/0_data.sql
@@ -17,12 +17,17 @@ CREATE TABLE IF NOT EXISTS headers (
     UNIQUE (msg_hash, name, value)
 );
 
+CREATE INDEX IF NOT EXISTS idx_headers_msg_hash ON headers(msg_hash);
+
+
 CREATE TABLE IF NOT EXISTS bodies (
     msg_hash TEXT NOT NULL,
     text TEXT NOT NULL,
     FOREIGN KEY (msg_hash) REFERENCES messages(hash),
     UNIQUE (msg_hash)
 );
+
+CREATE INDEX IF NOT EXISTS idx_bodies_msg_hash ON bodies(msg_hash);
 
 -------------------------------------------------------------------------------
 -- State:
@@ -33,3 +38,5 @@ CREATE TABLE IF NOT EXISTS last_seen_msg (
     uid INTEGER NOT NULL,
     UNIQUE (account, mailbox)
 );
+
+CREATE INDEX idx_last_seen_msg_account_mailbox ON last_seen_msg(account, mailbox);
